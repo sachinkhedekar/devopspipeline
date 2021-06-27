c9c76b3ee7308a0a08d5e65f2b0c00190100a1cb
sachinkhedekar <47321756+sachinkhedekar@users.noreply.github.com>
2021-06-27 15:56:16 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
-Class Hello
+class hello
 {
-        public static void main (string args []){
-                System.out.println("Hello Java");
-        }
+	public static void main (String args []){
+		System.out.println("Hello Java");
+	}
 }
